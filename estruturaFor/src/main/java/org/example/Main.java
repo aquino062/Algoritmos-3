807889eb24f4730bb19f9687b655479005513b0e
aquino062 <willian_wjunior@hotmail.com>
2023-04-22 09:14:50 -0300

    .

---
+++ estruturaFor/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+
+        for(int i = 0; i <10;i++){
+            System.out.println(("ola"));
+        }
+
+    }
+}
