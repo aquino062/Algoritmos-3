6e91cc7d432938c1275fafc3e567a56a1e94638a
aquino062 <willian_wjunior@hotmail.com>
2023-04-23 11:56:38 -0300

    1.0

---
+++ exercicio-22/src/main/java/org/example/Main.java
@@ -0,0 +1,48 @@
+package org.example;
+
+
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+       String nomeProduto;
+       float precoCusto;
+       float precoVenda;
+       float totalCusto = 0.0f;
+       float totalVenda = 0.0f;
+
+       Scanner lScanner = new Scanner(System.in);
+
+       int i = 0;
+        for (; i < 3  ; i++) {
+
+            System.out.println("Digite o nome do produto:");
+            nomeProduto = lScanner.next();
+
+            System.out.println("Digite o preço de custo do produto:");
+            precoCusto = lScanner.nextFloat();
+
+            System.out.println("Digite o preço de venda do produto:");
+            precoVenda = lScanner.nextFloat();
+
+
+            totalCusto = totalCusto + precoCusto;
+            totalVenda = totalVenda + precoVenda;
+
+
+            if( precoCusto == precoVenda){
+                System.out.println("houve um empate entre preço de custo e preço de venda.");
+            }else{
+                if(precoCusto > precoVenda){
+                    System.out.println("houve um prejuizo.");
+                }else {
+                    System.out.println("houve lucro.");
+                }
+            }
+            System.out.println(nomeProduto + " , preço de custo = " + precoCusto + " , preço de venda = "+ precoVenda);
+        }
+
+        System.out.println("a média do preço de custo é de: " + (totalCusto / i ));
+        System.out.println("a média do preço de venda é de: " + (totalVenda / i ));
+    }
+}
