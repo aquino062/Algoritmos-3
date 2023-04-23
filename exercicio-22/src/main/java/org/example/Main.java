590ad86e06709da42705f96f7e8a0554e863511e
aquino062 <willian_wjunior@hotmail.com>
2023-04-23 12:25:58 -0300

    ..

--- exercicio-22/src/main/java/org/example/Main.java
+++ exercicio-22/src/main/java/org/example/Main.java
@@ -1,20 +1,20 @@
 package org.example;
 
 
-
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-       String nomeProduto;
-       float precoCusto;
-       float precoVenda;
-       float totalCusto = 0.0f;
-       float totalVenda = 0.0f;
+        String nomeProduto;
+        float precoCusto;
+        float precoVenda;
+        float totalCusto = 0.0f;
+        float totalVenda = 0.0f;
 
-       Scanner lScanner = new Scanner(System.in);
+        Scanner lScanner = new Scanner(System.in);
 
-       int i = 0;
-        for (; i < 3  ; i++) {
+        int i = 0;
+        for (; i < 3; i++) {
 
             System.out.println("Digite o nome do produto:");
             nomeProduto = lScanner.next();
@@ -30,19 +30,31 @@ public class Main {
             totalVenda = totalVenda + precoVenda;
 
 
-            if( precoCusto == precoVenda){
+            if (precoCusto == precoVenda) {
                 System.out.println("houve um empate entre preço de custo e preço de venda.");
-            }else{
-                if(precoCusto > precoVenda){
+            } else {
+                if (precoCusto > precoVenda) {
                     System.out.println("houve um prejuizo.");
-                }else {
+                } else {
                     System.out.println("houve lucro.");
                 }
             }
-            System.out.println(nomeProduto + " , preço de custo = " + precoCusto + " , preço de venda = "+ precoVenda);
+            System.out.println(nomeProduto + " , preço de custo = " + precoCusto + " , preço de venda = " + precoVenda);
+
+            float lucroPorcentagem = ((precoVenda - precoCusto) / precoCusto) * 100;
+            // formula para calcular a porcentagem de lucro
+
+            System.out.println("Porcentagem de lucro de " + nomeProduto + ": " + lucroPorcentagem + "%");
+
         }
 
-        System.out.println("a média do preço de custo é de: " + (totalCusto / i ));
-        System.out.println("a média do preço de venda é de: " + (totalVenda / i ));
+        float mediaTotalCusto = totalCusto / i;
+        float mediaTotalVenda = totalVenda / i;
+
+        System.out.println("a média do preço de custo é de: " + mediaTotalCusto);
+        System.out.println("a média do preço de venda é de: " + mediaTotalVenda);
+        System.out.println("a margem de lucro foi de: " + ((mediaTotalVenda - mediaTotalCusto) / mediaTotalCusto * 100)
+                + "%");
+
     }
 }
