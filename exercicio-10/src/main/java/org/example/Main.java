6165a2a56f89d094ad8434e91e4bd6f50e5310ed
aquino062 <willian_wjunior@hotmail.com>
2023-04-26 09:04:24 -0300

    1.0

--- exercicio-10/src/main/java/org/example/Main.java
+++ exercicio-10/src/main/java/org/example/Main.java
@@ -1,9 +1,38 @@
 package org.example;
 
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        
+        Scanner scanner = new Scanner(System.in);
+
+        float valorProduto;
+        int numeroParcelas;
+
+        System.out.println("Digite o valor do produto:");
+        valorProduto = scanner.nextFloat();
+
+
+        do {
+        System.out.println("Digite o número de parcelas. (até em 5x sem juros)");
+        numeroParcelas = scanner.nextInt();
+
+            if (numeroParcelas > 5) {
+
+                System.out.println("você só pode parcelar até em 5x.");
+
+
+            } else {
+
+                System.out.println("o valor do produto ficou " + numeroParcelas + "x de " + (valorProduto / numeroParcelas));
+            }
+        }while(numeroParcelas > 5);
+
+
+
+
+
+
     }
 }
