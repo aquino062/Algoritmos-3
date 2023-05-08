25af3164328164b2579b7bd8917c7f557e1eac2e
willian aquino <willian_wjunior@hotmail.com>
2023-05-08 10:12:17 -0300

    2.0

--- exercicios-/exercicio-35/src/Main.java
+++ exercicios-/exercicio-35/src/Main.java
@@ -4,24 +4,25 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+
         double valorConta = 0;
 
-        System.out.println("Digite qntd de horas consumidas");
-        double horas = scanner.nextDouble();
-
-        System.out.println("Digite o tipo do imovel: \n 1- Residência \n 2- comércio \n 3- Indústria");
+        System.out.println("Digite o tipo do cliente: \n 1- Residência \n 2- comércio \n 3- Indústria");
         int tipo = scanner.nextInt();
+
+        System.out.println("Digite o consumo KW/h:");
+        double consumo = scanner.nextDouble();
 
 
         switch (tipo) {
             case 1:
-                valorConta = horas * 0.60;
+                valorConta = consumo * 0.60;
                 break;
             case 2:
-                valorConta = horas * 0.48;
+                valorConta = consumo * 0.48;
                 break;
             case 3:
-                valorConta = horas * 1.29;
+                valorConta = consumo * 1.29;
                 break;
             default:
                 System.out.println("opção inválida");
@@ -29,6 +30,7 @@ public class Main {
         }
 
         String valorFormatado = String.format("%.2f", valorConta);
-        System.out.println("o valor total a pagar foi de R$ " + valorFormatado);
+        System.out.println(" valor total a pagar R$ " + valorFormatado);
+        System.out.printf(" valor total a pagar R$ %.2f",valorConta);
     }
 }
